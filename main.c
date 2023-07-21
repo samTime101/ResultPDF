4f57909e8983a018cf9787f87e9ae288c89108f6
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-07-21 16:48:46 +0545

    working on the Main function

--- main.c
+++ main.c
@@ -4,20 +4,24 @@
 struct record
   {
   char name[20],school[40];
+  float per;
   
   }data;
 
 int main() {
 
+printf("****SAM_TIME101******\n");
+printf("****The functions will be added to the main program of QUICK QUIZ which i am working******\n");
   printf("Enter school name:");
-  //doesnot read more than 39 characters , yep much safe
+  //doesnot read more than 9 characters , yep much safe
   scanf(" %39[^\n]", data.school);
   
   printf("Enter name:");
-  //not more 9 characters , yep much safe
+  //doesnot read more than 9 characters , yep much safe
   scanf(" %19[^\n]", data.name);
   
   pdf(data.name,data.school);
 
+  return 0;
   
 }
